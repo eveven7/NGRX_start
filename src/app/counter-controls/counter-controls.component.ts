af2b701e849fc7932871f8602b01eaa763788af4
Evelina <evelina.venckute@swedbank.lt>
2023-06-15 09:30:15 +0300

    alternative way of ACTIONS

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.actions';
-
+import { IncrementAction } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -12,9 +11,10 @@ import { increment } from '../store/counter.actions';
 export class CounterControlsComponent {
   constructor(private store: Store) {}
 
-  increment() {this.store.dispatch(increment({value: 2}))
+  increment() {
+    // this.store.dispatch(increment({ value: 2 }));
+    this.store.dispatch(new IncrementAction(2))
   }
 
-  decrement() {
-  }
+  decrement() {}
 }
